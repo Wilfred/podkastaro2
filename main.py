f208c663640f09e7a52849d4d7f0bd59d0eb34cc
Wilfred Hughes <me@wilfred.me.uk>
2011-02-10 00:11:49 +0000

    adding list of currently active podcasts

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+# -*- coding: utf-8 -*-
 import feedparser
 
-podcast_url = "http://feeds.feedburner.com/vej"
+# active podcasts only
+podcasts = [('http://www2.polskieradio.pl/podcast/39/podcast.xml', 'Pola Radio'),
+            ('http://radioverda.squarespace.com/storage/audio/radioverda.xml', 'Radio Verda'),
+            ('http://www.podkasto.net/feed/', 'Varsovia Vento'),
+            ('http://parolumondo.com/?feed=podcast', 'Parolu Mondo'),
+            ('http://melburno.org.au/3ZZZradio/feed/', '3ZZZ Radio'),
+            ('http://www.ameriko.org/eo/rhc/feed', 'Radio Havana Kubo'),
+            ('http://la-ondo.rpod.ru/rss.xml', 'Radio Esperanto'),
+            ('http://radioaktiva.esperanto.org.uy/?feed=podcast', 'Radio Aktiva'),
+            ('http://media01.vatiradio.va/podmaker/podcaster.aspx?c=esperanto_1', 'Radio Vatikana'),
+            ('http://media01.vatiradio.va/podmaker/podcaster.aspx?c=esperanto_2', 'Radio Vatikana'),
+            ('http://podkastoperposhtelefono.posterous.com/rss.xml', u'Podkasto Per Poŝtelefono'),
+            ('http://media.radio-libertaire.org/php/emission.rss.php?emi=59', 'Radio ZAM'),
+            ('http://www.panamaradio.org/podkastoj/rss.xml', 'Panama Radio'),
+            ("http://feeds.feedburner.com/vej", u'Voĉoj el Japanio')]
 
-feed = feedparser.parse(podcast_url)
+feed = feedparser.parse(podcasts[-1][0])
 
 for entry in feed['entries']:
     title = entry['title']
