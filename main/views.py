8786c5b7f0668bdd2903d2b9a9d43aa8ffef4d30
Wilfred Hughes <me@wilfred.me.uk>
2011-02-19 15:19:04 +0000

    starting the styling work

--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def index(request):
-    return HttpResponse('hello world')
+    return render_to_response('index.html', {}, RequestContext(request))
+
