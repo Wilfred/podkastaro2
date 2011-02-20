69d5d6358d668cf7a905c9782d65932d45d2ec8b
Wilfred Hughes <me@wilfred.me.uk>
2011-02-20 14:58:00 +0000

    adding multimedia to admin

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
-from main.models import Podcast, RssFeed, Episode
+from main.models import Podcast, RssFeed, Episode, MultimediaFile
 from django.contrib import admin
 
 admin.site.register(Podcast)
 admin.site.register(RssFeed)
 admin.site.register(Episode)
+admin.site.register(MultimediaFile)
