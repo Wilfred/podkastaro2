903c1bf8804b28d06b8654c51fa8f9a5e753f12f
Wilfred Hughes <me@wilfred.me.uk>
2011-02-20 17:22:49 +0000

    adding an index page

--- main/views.py
+++ main/views.py
@@ -6,7 +6,17 @@ from models import Podcast, RssFeed, Episode, MultimediaFile
 
 
 def index(request):
-    return render_to_response('page.html', {}, RequestContext(request))
+    episodes = Episode.objects.order_by('-time')
+
+    episodes_with_multimedia = []
+    for episode in episodes:
+        episodes_with_multimedia.append((episode,
+                                         MultimediaFile.objects.filter(episode=episode)))
+
+    template_vars = {'episodes': episodes_with_multimedia}
+    
+    return render_to_response('index.html', template_vars,
+                              RequestContext(request))
 
 def podcast(request, podcast_name):
     podcast = Podcast.objects.get_by_slug(podcast_name)
