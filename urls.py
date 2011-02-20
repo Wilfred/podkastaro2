2d03aa2510aee39f0d1136ee09f42fea7880d37f
Wilfred Hughes <me@wilfred.me.uk>
2011-02-20 12:16:08 +0000

    enabling django admin

--- urls.py
+++ urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls.defaults import *
 
+from django.contrib import admin
+admin.autodiscover()
+
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
+    ('^admin', include(admin.site.urls)),
     ('^$', 'main.views.index'),
 )
