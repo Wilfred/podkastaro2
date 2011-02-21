6e6a70f8e5182107b93d22aa1c63d7c1af9e0f73
Wilfred Hughes <me@wilfred.me.uk>
2011-02-21 22:50:16 +0000

    refactoring admin model declarations

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,5 @@
 from main.models import Podcast, RssFeed, Episode, MultimediaFile
 from django.contrib import admin
 
-admin.site.register(Podcast)
-admin.site.register(RssFeed)
-admin.site.register(Episode)
-admin.site.register(MultimediaFile)
+for cls in [Podcast, RssFeed, Episode, MultimediaFile]:
+    admin.site.register(cls)
