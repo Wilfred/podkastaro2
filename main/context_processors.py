93ff70197edbea54cace71e4ac6bd84abb03da66
Wilfred Hughes <me@wilfred.me.uk>
2011-03-06 11:58:08 +0000

    sort podcasts alphabetically

--- main/context_processors.py
+++ main/context_processors.py
@@ -1,7 +1,11 @@
 from models import Podcast
+from utils import compare_esperanto_strings
 
 def list_podcasts(request):
     # add a list of all podcasts to every request
+    # sorted alphabetically according to the Esperanto alphabet
     podcasts = Podcast.objects.all()
+    podcasts = sorted(podcasts, cmp=compare_esperanto_strings,
+                      key=lambda p:p.name)
 
     return {'podcasts': podcasts}
