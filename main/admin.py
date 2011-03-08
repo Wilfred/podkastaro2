c52b9ae61f96a49af7c51ec6a4db7f522967f011
Wilfred Hughes <me@wilfred.me.uk>
2011-03-08 22:18:08 +0000

    adding columns to admin view of rss feed

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,7 @@
 from main.models import Podcast, RssFeed, Episode, MultimediaFile
 from django.contrib import admin
 
-for cls in [Podcast, RssFeed, MultimediaFile]:
+for cls in [Podcast, MultimediaFile]:
     admin.site.register(cls)
 
 class EpisodeAdmin(admin.ModelAdmin):
@@ -9,3 +9,8 @@ class EpisodeAdmin(admin.ModelAdmin):
     list_display = ('title', 'podcast', 'time')
 
 admin.site.register(Episode, EpisodeAdmin)
+
+class RssFeedAdmin(admin.ModelAdmin):
+    list_display = ('podcast', 'url')
+
+admin.site.register(RssFeed, RssFeedAdmin)
