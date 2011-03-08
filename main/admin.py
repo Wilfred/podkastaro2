469e74fdaac39b04d8f7526a30b3164c1e953abc
Wilfred Hughes <me@wilfred.me.uk>
2011-03-08 01:30:08 +0000

    adding some sort options and filters to the admin pages

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,11 @@
 from main.models import Podcast, RssFeed, Episode, MultimediaFile
 from django.contrib import admin
 
-for cls in [Podcast, RssFeed, Episode, MultimediaFile]:
+for cls in [Podcast, RssFeed, MultimediaFile]:
     admin.site.register(cls)
+
+class EpisodeAdmin(admin.ModelAdmin):
+    list_filter = ('podcast',)
+    list_display = ('title', 'podcast', 'time')
+
+admin.site.register(Episode, EpisodeAdmin)
