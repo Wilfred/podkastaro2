6deb455f6e557f0a3cf932e980eefacafcd33497
Wilfred Hughes <me@wilfred.me.uk>
2012-07-16 23:34:57 +0100

    shortening urls (of course they're podcasts on a podcast site), Stack Overflow URL style

--- urls.py
+++ urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
 
     url('^$', 'podcasts.views.index', name='index'),
     url('^pri-podkastaro$', 'podcasts.views.about', name='about'),
-    url('^podkasto/(?P<podcast_id>\d+)/(?P<podcast_name>.+)', 'podcasts.views.view_podcast', name="view_podcast"),
+    url('^p/(?P<podcast_id>\d+)/(?P<podcast_name>.+)', 'podcasts.views.view_podcast', name="view_podcast"),
 
     ('^cron/check_feeds', 'podcasts.views.check_feeds'),
 
