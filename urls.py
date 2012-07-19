02d597cd0e08dfe4833ba4ba5a50bdba4b817474
Wilfred Hughes <me@wilfred.me.uk>
2012-07-19 22:25:20 +0100

    adding 404 and 500 templates

--- urls.py
+++ urls.py
@@ -3,8 +3,6 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
-handler500 = 'djangotoolbox.errorviews.server_error'
-
 urlpatterns = patterns('',
     ('^admin/', include(admin.site.urls)),
 
